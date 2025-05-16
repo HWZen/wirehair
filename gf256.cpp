8a9878bc3388bd5ce3ee79c08facd7791f4310dd
HWZen <1041453977@qq.com>
2025-05-16 18:50:36 +0800

    Add build2 and .idea to .gitignore; update CMakeLists.txt with additional compiler flags; create compile_commands.json; refactor gf256.cpp for improved SIMD usage

--- gf256.cpp
+++ gf256.cpp
@@ -230,42 +230,42 @@ static bool CpuHasSSSE3 = false;
 #define CPUID_EBX_AVX2    0x00000020
 #define CPUID_ECX_SSSE3   0x00000200
 
-static void _cpuid(unsigned int cpu_info[4U], const unsigned int cpu_info_type)
-{
-#if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64) || defined(_M_IX86))
-    __cpuid((int *) cpu_info, cpu_info_type);
-#else //if defined(HAVE_CPUID)
-    cpu_info[0] = cpu_info[1] = cpu_info[2] = cpu_info[3] = 0;
-# ifdef __i386__
-    __asm__ __volatile__ ("pushfl; pushfl; "
-                          "popl %0; "
-                          "movl %0, %1; xorl %2, %0; "
-                          "pushl %0; "
-                          "popfl; pushfl; popl %0; popfl" :
-                          "=&r" (cpu_info[0]), "=&r" (cpu_info[1]) :
-                          "i" (0x200000));
-    if (((cpu_info[0] ^ cpu_info[1]) & 0x200000) == 0) {
-        return; /* LCOV_EXCL_LINE */
-    }
-# endif
-# ifdef __i386__
-    __asm__ __volatile__ ("xchgl %%ebx, %k1; cpuid; xchgl %%ebx, %k1" :
-                          "=a" (cpu_info[0]), "=&r" (cpu_info[1]),
-                          "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
-                          "0" (cpu_info_type), "2" (0U));
-# elif defined(__x86_64__)
-    __asm__ __volatile__ ("xchgq %%rbx, %q1; cpuid; xchgq %%rbx, %q1" :
-                          "=a" (cpu_info[0]), "=&r" (cpu_info[1]),
-                          "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
-                          "0" (cpu_info_type), "2" (0U));
-# else
-    __asm__ __volatile__ ("cpuid" :
-                          "=a" (cpu_info[0]), "=b" (cpu_info[1]),
-                          "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
-                          "0" (cpu_info_type), "2" (0U));
-# endif
-#endif
-}
+// static void _cpuid(unsigned int cpu_info[4U], const unsigned int cpu_info_type)
+// {
+// #if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64) || defined(_M_IX86))
+//     __cpuid((int *) cpu_info, cpu_info_type);
+// #else //if defined(HAVE_CPUID)
+//     cpu_info[0] = cpu_info[1] = cpu_info[2] = cpu_info[3] = 0;
+// # ifdef __i386__
+//     __asm__ __volatile__ ("pushfl; pushfl; "
+//                           "popl %0; "
+//                           "movl %0, %1; xorl %2, %0; "
+//                           "pushl %0; "
+//                           "popfl; pushfl; popl %0; popfl" :
+//                           "=&r" (cpu_info[0]), "=&r" (cpu_info[1]) :
+//                           "i" (0x200000));
+//     if (((cpu_info[0] ^ cpu_info[1]) & 0x200000) == 0) {
+//         return; /* LCOV_EXCL_LINE */
+//     }
+// # endif
+// # ifdef __i386__
+//     __asm__ __volatile__ ("xchgl %%ebx, %k1; cpuid; xchgl %%ebx, %k1" :
+//                           "=a" (cpu_info[0]), "=&r" (cpu_info[1]),
+//                           "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
+//                           "0" (cpu_info_type), "2" (0U));
+// # elif defined(__x86_64__)
+//     __asm__ __volatile__ ("xchgq %%rbx, %q1; cpuid; xchgq %%rbx, %q1" :
+//                           "=a" (cpu_info[0]), "=&r" (cpu_info[1]),
+//                           "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
+//                           "0" (cpu_info_type), "2" (0U));
+// # else
+//     __asm__ __volatile__ ("cpuid" :
+//                           "=a" (cpu_info[0]), "=b" (cpu_info[1]),
+//                           "=c" (cpu_info[2]), "=d" (cpu_info[3]) :
+//                           "0" (cpu_info_type), "2" (0U));
+// # endif
+// #endif
+// }
 
 #else
 #if defined(LINUX_ARM)
@@ -324,12 +324,14 @@ static void gf256_architecture_init()
 #if !defined(GF256_TARGET_MOBILE)
     unsigned int cpu_info[4];
 
-    _cpuid(cpu_info, 1);
-    CpuHasSSSE3 = ((cpu_info[2] & CPUID_ECX_SSSE3) != 0);
+    // _cpuid(cpu_info, 1);
+    // CpuHasSSSE3 = ((cpu_info[2] & CPUID_ECX_SSSE3) != 0);
+    CpuHasSSSE3 = true;
 
 #if defined(GF256_TRY_AVX2)
-    _cpuid(cpu_info, 7);
-    CpuHasAVX2 = ((cpu_info[1] & CPUID_EBX_AVX2) != 0);
+    // _cpuid(cpu_info, 7);
+    // CpuHasAVX2 = ((cpu_info[1] & CPUID_EBX_AVX2) != 0);
+    CpuHasAVX2 = true;
 #endif // GF256_TRY_AVX2
 
     // When AVX2 and SSSE3 are unavailable, Siamese takes 4x longer to decode
@@ -587,8 +589,8 @@ static void gf256_mul_mem_init()
         {
             const GF256_M256 table_lo2 = _mm256_broadcastsi128_si256(table_lo);
             const GF256_M256 table_hi2 = _mm256_broadcastsi128_si256(table_hi);
-            _mm256_storeu_si256(GF256Ctx.MM256.TABLE_LO_Y + y, table_lo2);
-            _mm256_storeu_si256(GF256Ctx.MM256.TABLE_HI_Y + y, table_hi2);
+            _mm256_storeu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_LO_Y + y), table_lo2);
+            _mm256_storeu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_HI_Y + y), table_hi2);
         }
 # endif // GF256_TRY_AVX2
 #endif // GF256_TARGET_MOBILE
@@ -715,23 +717,23 @@ extern "C" void gf256_add_mem(void * GF256_RESTRICT vx,
 
         while (bytes >= 128)
         {
-            GF256_M256 x0 = _mm256_loadu_si256(x32);
-            GF256_M256 y0 = _mm256_loadu_si256(y32);
+            GF256_M256 x0 = _mm256_loadu_si256((__m256i_u*)(x32));
+            GF256_M256 y0 = _mm256_loadu_si256((__m256i_u*)(y32));
             x0 = _mm256_xor_si256(x0, y0);
-            GF256_M256 x1 = _mm256_loadu_si256(x32 + 1);
-            GF256_M256 y1 = _mm256_loadu_si256(y32 + 1);
+            GF256_M256 x1 = _mm256_loadu_si256((__m256i_u*)(x32 + 1));
+            GF256_M256 y1 = _mm256_loadu_si256((__m256i_u*)(y32 + 1));
             x1 = _mm256_xor_si256(x1, y1);
-            GF256_M256 x2 = _mm256_loadu_si256(x32 + 2);
-            GF256_M256 y2 = _mm256_loadu_si256(y32 + 2);
+            GF256_M256 x2 = _mm256_loadu_si256((__m256i_u*)(x32 + 2));
+            GF256_M256 y2 = _mm256_loadu_si256((__m256i_u*)(y32 + 2));
             x2 = _mm256_xor_si256(x2, y2);
-            GF256_M256 x3 = _mm256_loadu_si256(x32 + 3);
-            GF256_M256 y3 = _mm256_loadu_si256(y32 + 3);
+            GF256_M256 x3 = _mm256_loadu_si256((__m256i_u*)(x32 + 3));
+            GF256_M256 y3 = _mm256_loadu_si256((__m256i_u*)(y32 + 3));
             x3 = _mm256_xor_si256(x3, y3);
 
-            _mm256_storeu_si256(x32, x0);
-            _mm256_storeu_si256(x32 + 1, x1);
-            _mm256_storeu_si256(x32 + 2, x2);
-            _mm256_storeu_si256(x32 + 3, x3);
+            _mm256_storeu_si256((__m256i_u*)(x32), x0);
+            _mm256_storeu_si256((__m256i_u*)(x32 + 1), x1);
+            _mm256_storeu_si256((__m256i_u*)(x32 + 2), x2);
+            _mm256_storeu_si256((__m256i_u*)(x32 + 3), x3);
 
             bytes -= 128, x32 += 4, y32 += 4;
         }
@@ -740,10 +742,10 @@ extern "C" void gf256_add_mem(void * GF256_RESTRICT vx,
         while (bytes >= 32)
         {
             // x[i] = x[i] xor y[i]
-            _mm256_storeu_si256(x32,
+            _mm256_storeu_si256((__m256i_u*)(x32),
                 _mm256_xor_si256(
-                    _mm256_loadu_si256(x32),
-                    _mm256_loadu_si256(y32)));
+                    _mm256_loadu_si256((__m256i_u*)(x32)),
+                    _mm256_loadu_si256((__m256i_u*)(y32))));
 
             bytes -= 32, ++x32, ++y32;
         }
@@ -880,12 +882,12 @@ extern "C" void gf256_add2_mem(void * GF256_RESTRICT vz, const void * GF256_REST
         const unsigned count = bytes / 32;
         for (unsigned i = 0; i < count; ++i)
         {
-            _mm256_storeu_si256(z32 + i,
+            _mm256_storeu_si256((__m256i_u*)(z32 + i),
                 _mm256_xor_si256(
-                    _mm256_loadu_si256(z32 + i),
+                    _mm256_loadu_si256((__m256i_u*)(z32 + i)),
                     _mm256_xor_si256(
-                        _mm256_loadu_si256(x32 + i),
-                        _mm256_loadu_si256(y32 + i))));
+                        _mm256_loadu_si256((__m256i_u*)(x32 + i)),
+                        _mm256_loadu_si256((__m256i_u*)(y32 + i)))));
         }
 
         bytes -= count * 32;
@@ -1017,10 +1019,10 @@ extern "C" void gf256_addset_mem(void * GF256_RESTRICT vz, const void * GF256_RE
         const unsigned count = bytes / 32;
         for (unsigned i = 0; i < count; ++i)
         {
-            _mm256_storeu_si256(z32 + i,
+            _mm256_storeu_si256((__m256i_u*)(z32 + i),
                 _mm256_xor_si256(
-                    _mm256_loadu_si256(x32 + i),
-                    _mm256_loadu_si256(y32 + i)));
+                    _mm256_loadu_si256((__m256i_u*)(x32 + i)),
+                    _mm256_loadu_si256((__m256i_u*)(y32 + i))));
         }
 
         bytes -= count * 32;
@@ -1148,8 +1150,8 @@ extern "C" void gf256_mul_mem(void * GF256_RESTRICT vz, const void * GF256_RESTR
     if (bytes >= 32 && CpuHasAVX2)
     {
         // Partial product tables; see above
-        const GF256_M256 table_lo_y = _mm256_loadu_si256(GF256Ctx.MM256.TABLE_LO_Y + y);
-        const GF256_M256 table_hi_y = _mm256_loadu_si256(GF256Ctx.MM256.TABLE_HI_Y + y);
+        const GF256_M256 table_lo_y = _mm256_loadu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_LO_Y + y));
+        const GF256_M256 table_hi_y = _mm256_loadu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_HI_Y + y));
 
         // clr_mask = 0x0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f
         const GF256_M256 clr_mask = _mm256_set1_epi8(0x0f);
@@ -1161,13 +1163,13 @@ extern "C" void gf256_mul_mem(void * GF256_RESTRICT vz, const void * GF256_RESTR
         do
         {
             // See above comments for details
-            GF256_M256 x0 = _mm256_loadu_si256(x32);
+            GF256_M256 x0 = _mm256_loadu_si256((__m256i_u*)(x32));
             GF256_M256 l0 = _mm256_and_si256(x0, clr_mask);
             x0 = _mm256_srli_epi64(x0, 4);
             GF256_M256 h0 = _mm256_and_si256(x0, clr_mask);
             l0 = _mm256_shuffle_epi8(table_lo_y, l0);
             h0 = _mm256_shuffle_epi8(table_hi_y, h0);
-            _mm256_storeu_si256(z32, _mm256_xor_si256(l0, h0));
+            _mm256_storeu_si256((__m256i_u*)(z32), _mm256_xor_si256(l0, h0));
 
             bytes -= 32, ++x32, ++z32;
         } while (bytes >= 32);
@@ -1314,8 +1316,8 @@ extern "C" void gf256_muladd_mem(void * GF256_RESTRICT vz, uint8_t y,
     if (bytes >= 32 && CpuHasAVX2)
     {
         // Partial product tables; see above
-        const GF256_M256 table_lo_y = _mm256_loadu_si256(GF256Ctx.MM256.TABLE_LO_Y + y);
-        const GF256_M256 table_hi_y = _mm256_loadu_si256(GF256Ctx.MM256.TABLE_HI_Y + y);
+        const GF256_M256 table_lo_y = _mm256_loadu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_LO_Y + y));
+        const GF256_M256 table_hi_y = _mm256_loadu_si256((__m256i_u*)(GF256Ctx.MM256.TABLE_HI_Y + y));
 
         // clr_mask = 0x0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f
         const GF256_M256 clr_mask = _mm256_set1_epi8(0x0f);
@@ -1328,25 +1330,25 @@ extern "C" void gf256_muladd_mem(void * GF256_RESTRICT vz, uint8_t y,
         for (unsigned i = 0; i < count; ++i)
         {
             // See above comments for details
-            GF256_M256 x0 = _mm256_loadu_si256(x32 + i * 2);
+            GF256_M256 x0 = _mm256_loadu_si256((__m256i_u*)(x32 + i * 2));
             GF256_M256 l0 = _mm256_and_si256(x0, clr_mask);
             x0 = _mm256_srli_epi64(x0, 4);
-            const GF256_M256 z0 = _mm256_loadu_si256(z32 + i * 2);
+            const GF256_M256 z0 = _mm256_loadu_si256((__m256i_u*)(z32 + i * 2));
             GF256_M256 h0 = _mm256_and_si256(x0, clr_mask);
             l0 = _mm256_shuffle_epi8(table_lo_y, l0);
             h0 = _mm256_shuffle_epi8(table_hi_y, h0);
             const GF256_M256 p0 = _mm256_xor_si256(l0, h0);
-            _mm256_storeu_si256(z32 + i * 2, _mm256_xor_si256(p0, z0));
+            _mm256_storeu_si256((__m256i_u*)(z32 + i * 2), _mm256_xor_si256(p0, z0));
 
-            GF256_M256 x1 = _mm256_loadu_si256(x32 + i * 2 + 1);
+            GF256_M256 x1 = _mm256_loadu_si256((__m256i_u*)(x32 + i * 2 + 1));
             GF256_M256 l1 = _mm256_and_si256(x1, clr_mask);
             x1 = _mm256_srli_epi64(x1, 4);
-            const GF256_M256 z1 = _mm256_loadu_si256(z32 + i * 2 + 1);
+            const GF256_M256 z1 = _mm256_loadu_si256((__m256i_u*)(z32 + i * 2 + 1));
             GF256_M256 h1 = _mm256_and_si256(x1, clr_mask);
             l1 = _mm256_shuffle_epi8(table_lo_y, l1);
             h1 = _mm256_shuffle_epi8(table_hi_y, h1);
             const GF256_M256 p1 = _mm256_xor_si256(l1, h1);
-            _mm256_storeu_si256(z32 + i * 2 + 1, _mm256_xor_si256(p1, z1));
+            _mm256_storeu_si256((__m256i_u*)(z32 + i * 2 + 1), _mm256_xor_si256(p1, z1));
         }
         bytes -= count * 64;
         z32 += count * 2;
@@ -1354,15 +1356,15 @@ extern "C" void gf256_muladd_mem(void * GF256_RESTRICT vz, uint8_t y,
 
         if (bytes >= 32)
         {
-            GF256_M256 x0 = _mm256_loadu_si256(x32);
+            GF256_M256 x0 = _mm256_loadu_si256((__m256i_u*)(x32));
             GF256_M256 l0 = _mm256_and_si256(x0, clr_mask);
             x0 = _mm256_srli_epi64(x0, 4);
             GF256_M256 h0 = _mm256_and_si256(x0, clr_mask);
             l0 = _mm256_shuffle_epi8(table_lo_y, l0);
             h0 = _mm256_shuffle_epi8(table_hi_y, h0);
             const GF256_M256 p0 = _mm256_xor_si256(l0, h0);
-            const GF256_M256 z0 = _mm256_loadu_si256(z32);
-            _mm256_storeu_si256(z32, _mm256_xor_si256(p0, z0));
+            const GF256_M256 z0 = _mm256_loadu_si256((__m256i_u*)(z32));
+            _mm256_storeu_si256((__m256i_u*)(z32), _mm256_xor_si256(p0, z0));
 
             bytes -= 32;
             z32++;
